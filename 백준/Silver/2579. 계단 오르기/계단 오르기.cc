bb8d6608ec71285c4128e0cb0ddc4302fa42bb63
tmdahr <p84800384@gmail.com>
2025-04-01 22:47:19 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int _max(int a,int b)
+{
+	if(a<b)
+		return b;
+	return a;
+}
+
+int stair[305];
+int dp[305];//dp[x]  x번째 계단으로 왔을때 최대값 
+
+
+
+int main(void)
+{
+	int last;
+	scanf("%d", &last);
+	for(int i=1; i<=last; i++) scanf("%d", &stair[i]);
+	
+	dp[1]=stair[1];
+	dp[2]=stair[1]+stair[2];
+	dp[3]=_max(stair[1]+stair[3],stair[2]+stair[3]);
+	for(int i=4;i<=last;i++)
+	{
+		dp[i]=_max(dp[i-2]+stair[i],  dp[i-3]+stair[i-1]+stair[i]);
+	}
+	printf("%d",dp[last]);
+	return 0;
+}
