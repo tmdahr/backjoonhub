0b8ca2a77b9ca2b45e7637a4c8abf11f96550aa1
tmdahr <p84800384@gmail.com>
2025-04-03 19:21:53 +0900

    [Silver III] Title: N과 M (7), Time: 516 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+int num[10], visit[10], arr[10];
+
+void back(int stop)
+{
+	if(stop==m)
+	{
+		for(int i=0; i<stop; i++) printf("%d ", arr[i]);
+		printf("\n");
+		return;
+	}
+	for(int i=0; i<n; i++)
+	{
+		arr[stop]=num[i];
+		back(stop+1);
+	}
+}
+
+int main(void)
+{
+	scanf("%d %d", &n, &m);
+	for(int i=0; i<n; i++) scanf("%d", &num[i]);
+	sort(num,num+n);
+	back(0);
+}
