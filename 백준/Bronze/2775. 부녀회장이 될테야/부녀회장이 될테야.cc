2bee5bc842ff58f6042eef36de2e2b2f7ac77bed
tmdahr <p84800384@gmail.com>
2025-05-22 00:02:56 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -1,26 +1,18 @@
 #include <stdio.h>
- 
-int main(){
+int main(void)
+{
+    int map[15][15]={0,};
+    
+    for(int i=0; i<15; i++) map[0][i]=i;
+    for(int i=1; i<15; i++) for(int j=1; j<15; j++) map[i][j]=map[i-1][j]+map[i][j-1];
 
-  int arr[15][15] = {0,};  // 아파트의 층은 14보다 작거나 같기때문에 각각 15크기의 2중배열을 선언하고 0으로 초기화.
-  int test,h,w;
+    int t, k, n;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++)
+    {
+        scanf("%d %d", &k, &n);
+        printf("%d\n", map[k][n]);
+    }
 
-  for(int i=0; i<15; i++){
-    arr[0][i] = i;  // 문제를 보면 0층의 i호에는 i명만큼 산다고해서 0층에는 i명으로 선언.
-  }
-  
-  for(int i=1; i<15; i++){
-    for(int j=1; j<15; j++){
-      arr[i][j] = arr[i-1][j] + arr[i][j-1];
-    }  // 문제에서 말한 공식대로 밑에층의 1호부터 해당호까지 사람들의 합을 arr[i][j]에 넣어준다.
-  }
-
-  scanf("%d",&test); 
-  
-  for(int i=0;i<test;i++){
-    scanf("%d %d", &h, &w);  
-    printf("%d\n", arr[h][w]);  
-  }
-
-	return 0; 
+    return 0;
 }
