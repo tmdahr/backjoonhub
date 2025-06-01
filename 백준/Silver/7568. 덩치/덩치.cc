00216714b8903c247e5c08a6c96206e64f8be8ef
tmdahr <p84800384@gmail.com>
2025-06-02 00:36:16 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.cc
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -1,23 +1,16 @@
 #include <stdio.h>
-
-int main() {
-    int N, x[50] = {0, }, y[50] = {0, }, rank;
-    
-    scanf("%d", &N);
-    
-    for (int i = 0; i < N; i++) {
-        scanf("%d %d", &x[i], &y[i]);
-    }
-    
-    for (int i = 0; i < N; i++) {
+int main(void)
+{
+    int n, x[50], y[50], rank;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) scanf("%d %d", &x[i], &y[i]);
+    for(int i = 0; i < n; i++)
+    {
         rank = 1;
-        
-        for (int j = 0; j < N; j++) {
-            if (x[i] < x[j] && y[i] < y[j]) {
-                rank++;
-            }
+        for(int j = 0; j < n; j++)
+        {
+            if(x[i] < x[j] && y[i] < y[j]) rank++;
         }
-        
         printf("%d ", rank);
     }
     
