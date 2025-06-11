052119e1911768b6f17f4496c531f49a7cf2368b
tmdahr <p84800384@gmail.com>
2025-06-11 22:34:38 +0900

    [Gold IV] Title: 최단경로, Time: 128 ms, Memory: 8352 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-typedef struct _node
+typedef struct
 {
 	int to, cost;
 } node;
@@ -16,45 +16,44 @@ struct cmp
 };
 priority_queue<node, vector<node>, cmp> pq;
 vector<node> map[20005];
-int dist[20005];
-bool visit[20005];
+int dist[20005], visit[20005];
 
 int main(void)
 {
-	int v,e,k;
-	scanf("%d %d %d", &v, &e, &k);
+	int v, e, k;
+	scanf("%d %d", &v, &e);
+	scanf("%d", &k);
+	for(int i=1; i<=v; i++) dist[i] = 2147483647;
 	for(int i=0; i<e; i++)
 	{
-		int from, to, cost;
-		scanf("%d %d %d", &from, &to, &cost);
-		map[from].push_back({to, cost});
+		int u,v,w;
+		scanf("%d %d %d", &u, &v, &w);
+		map[u].push_back({v,w});
 	}
-	for(int i=1; i<=v; i++)
-		dist[i]=2147483647;
-	
 	pq.push({k,0});
-	dist[k]=0;
+	dist[k] = 0;
 	while(!pq.empty())
 	{
 		node now = pq.top();
 		pq.pop();
 		if(visit[now.to]==true) continue;
 		else visit[now.to] = true;
-		
 		for(int i=0; i<map[now.to].size(); i++)
 		{
 			int next = map[now.to][i].to;
 			int cost = map[now.to][i].cost;
-			if(dist[next]>cost+now.cost)
+			if(dist[next] > now.cost+cost)
 			{
-				dist[next] = cost+now.cost;
-				pq.push({next, cost+now.cost});
+				dist[next] = now.cost+cost;
+				pq.push({next, dist[next]});
 			}
 		}
 	}
 	for(int i=1; i<=v; i++)
+	{
 		if(dist[i]==2147483647) printf("INF\n");
 		else printf("%d\n", dist[i]);
-	
+	}
+
 	return 0;
 }
