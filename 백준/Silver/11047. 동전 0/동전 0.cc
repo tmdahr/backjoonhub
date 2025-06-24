d669d73aa3f6236f0dfaf684dc0dcf8ca8d557b3
tmdahr <p84800384@gmail.com>
2025-06-25 08:19:18 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -2,24 +2,20 @@
 
 int arr[10];
 
-int main(){
+int main(void)
+{
     int n,k,ans = 0;
     int sum = 0;
-
     scanf("%d %d",&n,&k);
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
 
-    for(int i=0;i<n;i++)
-        scanf("%d",&arr[i]);
-
-    for(int i=n-1;i>=1;i--){
+    for(int i=n-1; i>=1; i--)
+    {
         int cnt = (k-sum)/arr[i];
-
         ans += cnt;
         sum += cnt*arr[i];
     }
-
     ans += k-sum;
-
     printf("%d",ans);
 
     return 0;
