afbef6851adff10662361bb7f3d77f43a2392666
tmdahr <p84800384@gmail.com>
2025-06-25 07:36:48 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 60 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,49 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
-{
+typedef struct {
     int x;
     int y;
-} Point;
+} point;
 
-int compare(const void *a, const void *b)
-{
-    Point A = *(Point *)a;
-    Point B = *(Point *)b;
-    if (A.y > B.y)
+int compare(const void* a, const void* b) {
+    const point* A = (const point*)a;
+    const point* B = (const point*)b;
+
+    if (A->y > B->y) return 1;
+    else if(A->y == B->y)
     {
-        return 1;
+        if (A->x > B->x) return 1;
+        else return -1;
     }
-    else if (A.y == B.y)
-    {
-        if (A.x > B.x)
-        {
-            return 1;
-        }
-        else
-        {
-            return -1;
-        }
-    }
-    return -1;
+    else  return -1;
 }
 
-int main()
-{
+int main(void) {
     int test;
     scanf("%d", &test);
-    Point *arr;
-    arr = (Point *)malloc(sizeof(Point) * test);
+
+    point* arr = (point*)malloc(sizeof(point) * test);
+
     for (int i = 0; i < test; i++)
-    {
         scanf("%d %d", &arr[i].x, &arr[i].y);
-    }
-    qsort(arr, test, sizeof(Point), compare);
+
+    qsort(arr, test, sizeof(point), compare);
+
     for (int i = 0; i < test; i++)
-    {
         printf("%d %d\n", arr[i].x, arr[i].y);
-    }
+
     free(arr);
     return 0;
 }
