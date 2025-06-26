06834b9c11a6b64811ca0ce074963cb36fcb2337
tmdahr <p84800384@gmail.com>
2025-06-27 08:19:35 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 1120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -4,19 +4,17 @@ using namespace std;
 
 int arr[1005];
 
-int main(){
+int main(void)
+{
     int n;
     int ans = 0;
 
     scanf("%d",&n);
-
-    for(int i=0;i<n;i++)
-        scanf("%d",&arr[i]);
+    for(int i=0; i<n; i++) scanf("%d",&arr[i]);
 
     sort(arr,arr+n);
 
-    for(int i=0;i<n;i++)
-        ans += arr[i]*(n-i);
+    for(int i=0; i<n; i++) ans += arr[i]*(n-i);
 
     printf("%d",ans);
 
