d0a6510a5bb45d730fac0a5dbae083badf15e016
tmdahr <p84800384@gmail.com>
2025-06-27 08:23:35 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,21 +1,16 @@
 #include <stdio.h>
  
-int main() {
-    int dp[11] = { 0 };
-    //초깃값
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 4;
+int main(void)
+{
+    int dp[11] = {0,1,2,4};
  
-    //각 숫자마다 방법의 수 구하기
-    for (int i = 4; i < 11; i++)
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    for(int i=4; i < 11; i++) dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
  
-    int T, n;
-    scanf("%d", &T);
+    int t, n;
+    scanf("%d", &t);
  
-    //결과 출력
-    for (int test_case = 0; test_case < T; test_case++) {
+    for(int tc = 0; tc < t; tc++)
+    {
         scanf("%d", &n);
         printf("%d\n", dp[n]);
     }
