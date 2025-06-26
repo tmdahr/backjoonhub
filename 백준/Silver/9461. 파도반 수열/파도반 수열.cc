a4810095fa60af323ab3a809189e9a6a2bc2e459
tmdahr <p84800384@gmail.com>
2025-06-27 08:26:02 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,15 +1,16 @@
 #include<stdio.h>
  
-long int tri[101] = { 0,1,1,1,2,2 };    // tri[5]까지 초기화
+long int tri[101] = {0,1,1,1,2,2};    // tri[5]까지 초기화
  
-int main() {
-    int T, N, t, i;
-    scanf("%d", &T);
-    for (t = 0; t < T; t++) {
-        scanf("%d", &N);
-        for (i = 6; i <= N; i++)
-            tri[i] = tri[i - 5] + tri[i - 1];
-        printf("%ld\n", tri[N]);
+int main(void)
+{
+    int t, n;
+    scanf("%d", &t);
+    for(; t>0; t--)
+    {
+        scanf("%d", &n);
+        for(int i=6; i <= n; i++) tri[i] = tri[i-5]+tri[i-1];
+        printf("%ld\n", tri[n]);
     }
 	return 0;
 }
