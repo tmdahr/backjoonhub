f2df9c387c93e4f054e0d3cfe0bd1c62eb152845
tmdahr <p84800384@gmail.com>
2025-06-27 08:15:22 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 120 ms, Memory: 6780 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -5,46 +5,47 @@
 #define MAX_SITE 100000
 #define MAX_LENGTH 21
 
-typedef struct {
+typedef struct
+{
     char site[MAX_LENGTH];
     char password[MAX_LENGTH];
 } Entry;
 
-int compare(const void* a, const void* b) {
+int compare(const void* a, const void* b)
+{
     return strcmp(((Entry*)a)->site, ((Entry*)b)->site);
 }
 
 Entry entries[MAX_SITE];
 
-int binary_search(char* key, int n) {
-    int left = 0, right = n - 1;
-    while (left <= right) {
+int binary_search(char* key, int n)
+{
+    int left = 0, right = n-1;
+    while(left <= right)
+    {
         int mid = (left + right) / 2;
         int cmp = strcmp(entries[mid].site, key);
-        if (cmp == 0) return mid;
-        if (cmp < 0) left = mid + 1;
+        if(cmp == 0) return mid;
+        if(cmp < 0) left = mid + 1;
         else right = mid - 1;
     }
     return -1;
 }
 
-int main() {
-    int N, M;
-    scanf("%d %d", &N, &M);
+int main(void)
+{
+    int n,m;
+    scanf("%d %d", &n, &m);
 
-    for (int i = 0; i < N; i++) {
-        scanf("%s %s", entries[i].site, entries[i].password);
-    }
-
-    qsort(entries, N, sizeof(Entry), compare);
+    for(int i=0; i < n; i++) scanf("%s %s", entries[i].site, entries[i].password);
+    qsort(entries, n, sizeof(Entry), compare);
 
     char query[MAX_LENGTH];
-    for (int i = 0; i < M; i++) {
+    for(int i = 0; i < m; i++)
+    {
         scanf("%s", query);
-        int idx = binary_search(query, N);
-        if (idx != -1) {
-            printf("%s\n", entries[idx].password);
-        }
+        int idx = binary_search(query, n);
+        if(idx != -1) printf("%s\n", entries[idx].password);
     }
 
     return 0;
