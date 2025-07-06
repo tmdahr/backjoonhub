abbce8ef1ddafa80c9d559a1bbf8f4bfa0fffc63
tmdahr <p84800384@gmail.com>
2025-07-06 23:55:36 +0900

    [Gold I] Title: 하노이 탑, Time: 12 ms, Memory: 1892 KB -BaekjoonHub

--- 백준/Gold/2270. 하노이 탑/하노이 탑.cc
+++ 백준/Gold/2270. 하노이 탑/하노이 탑.cc
@@ -2,49 +2,50 @@
 #define MOD 1000000
 
 int num[100001], pos[100001];
-int n, x, ans;
+int ans;
 
-void cal(int n) {
+void cal(int n)
+{
     num[0] = 1;
-    for (int i = 1; i <= n; i++)
-        num[i] = (num[i - 1] * 2) % MOD;
+    for(int i=1; i<=n; i++) num[i] = (num[i-1] * 2) % MOD;
 }
 
-void hanoi(int disc, int to) {
-    if (disc == 0) return;
+void hanoi(int m, int to)
+{
+    if(m == 0) return;
 
-    int now = pos[disc];
-    int sub = 6 - now - to;  // 보조 기둥 계산 (1+2+3=6)
+    int now = pos[m];
 
-    if (now == to) {
-        hanoi(disc - 1, to);
-    } else {
-        ans = (ans + num[disc - 1]) % MOD;
-        hanoi(disc - 1, sub);
+    if(now == to) hanoi(m-1, to);
+    else
+    {
+        ans = (ans + num[m-1]) % MOD;
+        hanoi(m-1, 6-now-to);
     }
 }
 
-int main() {
+int main(void)
+{
+    int n, a, b, c, m;
+    
     scanf("%d", &n);
     cal(n);
+    scanf("%d %d %d", &a, &b, &c);
 
-    int pole1, pole2, pole3;
-    scanf("%d %d %d", &pole1, &pole2, &pole3);
-
-    // 1번 기둥 디스크 입력
-    for (int i = 0; i < pole1; i++) {
-        scanf("%d", &x);
-        pos[x] = 1;
+    for(int i=0; i<a; i++)
+    {
+        scanf("%d", &m);
+        pos[m] = 1;
     }
-    // 2번 기둥 디스크 입력
-    for (int i = 0; i < pole2; i++) {
-        scanf("%d", &x);
-        pos[x] = 2;
+    for(int i=0; i<b; i++)
+    {
+        scanf("%d", &m);
+        pos[m] = 2;
     }
-    // 3번 기둥 디스크 입력
-    for (int i = 0; i < pole3; i++) {
-        scanf("%d", &x);
-        pos[x] = 3;
+    for(int i=0; i<c; i++)
+    {
+        scanf("%d", &m);
+        pos[m] = 3;
     }
 
     hanoi(n, pos[n]);
