1968b28b7d0528f83007f10dd6fb3fa73d706622
tmdahr <p84800384@gmail.com>
2025-08-20 07:09:04 +0900

    [Gold IV] Title: 고층 건물, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.cc
+++ 백준/Gold/1027. 고층 건물/고층 건물.cc
@@ -4,16 +4,19 @@ int N;
 long long h[55];
 int cnt[55];
 
-int main() {
+int main(void)
+{
     scanf("%d", &N);
-    for (int i = 0; i < N; i++)
-        scanf("%lld", &h[i]);
+    for(int i=0; i<N; i++) scanf("%lld", &h[i]);
 
-    for (int i = 0; i < N; i++) {
+    for(int i=0; i<N; i++)
+    {
         double max_slope = -1e18;
-        for (int j = i + 1; j < N; j++) {
-            double slope = (double)(h[j] - h[i]) / (j - i);
-            if (slope > max_slope) {
+        for(int j=i+1; j<N; j++)
+        {
+            double slope = (double)(h[j]-h[i]) / (j-i);
+            if(slope > max_slope)
+            {
                 max_slope = slope;
                 cnt[i]++;
                 cnt[j]++;
@@ -22,8 +25,9 @@ int main() {
     }
 
     int ans = 0;
-    for (int i = 0; i < N; i++) {
-        if (cnt[i] > ans) ans = cnt[i];
+    for(int i=0; i<N; i++)
+    {
+        if(cnt[i] > ans) ans = cnt[i];
     }
     printf("%d\n", ans);
 
