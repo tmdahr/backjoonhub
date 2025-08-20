dfb332ff13fe07373b000a9e6af2357f18e9a036
tmdahr <p84800384@gmail.com>
2025-08-20 12:22:40 +0900

    [Gold IV] Title: 스도쿠, Time: 184 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -1,81 +1,86 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct _node
 {
-	int h,w;
+    int h, w;
 } node;
-int map[10][10];
-node o[100];
-int c=0, end=0;
 
-bool ck1(int h, int w, int num)
+int map[10][10];
+node zero[100];
+int cnt=0;
+bool end = false;
+
+bool ch1(int h, int w, int num)
 {
-	for(int i=0; i<9; i++)
-	{
-		if(map[h][i]==num) return false;
-	}
-	return true;
+    for(int i=0; i<9; i++)
+    {
+        if(map[h][i]==num) return false;
+    }
+    return true;
 }
-bool ck2(int h, int w, int num)
+
+bool ch2(int h, int w, int num)
 {
-	for(int i=0; i<9; i++)
-	{
-		if(map[i][w]==num) return false;
-	}
-	return true;
+    for(int i=0; i<9; i++)
+    {
+        if(map[i][w]==num) return false;
+    }
+    return true;
 }
-bool ck3(int h, int w, int num)
+
+bool ch3(int h, int w, int num)
 {
-	int hh = h/3*3;
-	int ww = w/3*3;
-	for(int i=hh; i<hh+3; i++)
-	{
-		for(int j=ww; j<ww+3; j++)
-		{
-			if(map[i][j]==num) return false;
-		}
-	}
-	return true;
+    int hh = h/3*3;
+    int ww = w/3*3;
+    for(int i=0; i<3; i++)
+        for(int j=0; j<3; j++)
+            if(map[hh+i][ww+j]==num) return false;
+    return true;
 }
 
 void solution(int n)
 {
-	if(n==c)
-	{
-		end=1;
-		for(int i=0; i<9; i++)
-		{
-			for(int j=0; j<9; j++)
-			{
-				printf("%d ", map[i][j]);
-			}
-			puts("");
-		}
-		return;
-	}
-	
-	int hh=o[n].h;
-	int ww=o[n].w;
-	for(int i=1; i<=9; i++)
-	{
-		if(ck1(hh,ww,i)&&ck2(hh,ww,i)&&ck3(hh,ww,i))
-		{
-			map[hh][ww]=i;
-			solution(n+1);
-			if(end==1) break;
-			map[hh][ww]=0;
-		}
-	}
+    if(n==cnt)
+    {
+        end = true;
+        for(int i=0; i<9; i++)
+        {
+            for(int j=0; j<9; j++)
+                printf("%d ", map[i][j]);
+            printf("\n");
+        }
+        return ;
+    }
+
+    int nh = zero[n].h;
+    int nw = zero[n].w;
+    for(int i=1; i<=9; i++)
+    {
+        if(ch1(nh,nw,i) && ch2(nh,nw,i) && ch3(nh,nw,i))
+        {
+            map[nh][nw] = i;
+            solution(n+1);
+            if(end) return;
+            map[nh][nw] = 0;
+        }
+    }
 }
 
 int main(void)
 {
-	for(int i=0; i<9; i++)
-		for(int j=0; j<9; j++)
-		{
-			scanf("%d", &map[i][j]);
-			if(map[i][j]==0) o[c++] = {i,j};
-		}
-	solution(0);
-	return 0;
+    for(int i=0; i<9; i++)
+        for(int j=0; j<9; j++)
+        {
+            scanf("%d", &map[i][j]);
+            if(map[i][j]==0)
+            {
+                zero[cnt].h = i;
+                zero[cnt].w = j;
+                cnt++;
+            }
+        }
+    solution(0);
+
+    return 0;
 }
