7e066852ebd2c526253670a6fc2a0cef1b02e19a
tmdahr <p84800384@gmail.com>
2025-08-22 08:18:41 +0900

    [Gold V] Title: Coins, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.cc
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct node
+{
+    int sum, num;
+};
+
+int main(void)
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        int n, m, count=0;
+        int coin[25];
+        int dp[10001]={0};
+        
+        scanf("%d", &n);
+        for(int i=0; i<n; i++) scanf("%d", &coin[i]);
+        scanf("%d", &m);
+
+        dp[0]=1;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=coin[i]; j<=m; j++)
+            {
+                dp[j] += dp[j-coin[i]];
+            }
+        }
+        printf("%d\n", dp[m]);
+    }
+    
+    return 0;
+}
