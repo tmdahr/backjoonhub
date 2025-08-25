c36a9aac60c4522a20374ad47d1bfe2b31c0f31b
tmdahr <p84800384@gmail.com>
2025-08-26 03:05:42 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+
+    int dp[1001] = {0};
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 0;
+    dp[4] = 1;
+    
+    for(int i=5; i<=N; i++)
+    {
+        if(dp[i-1] == 0 || dp[i-3] == 0 || dp[i-4] == 0) dp[i] = 1;
+        else dp[i] = 0;
+    }
+
+    if(dp[N] == 1) printf("SK\n");
+    else printf("CY\n");
+
+    return 0;
+}
