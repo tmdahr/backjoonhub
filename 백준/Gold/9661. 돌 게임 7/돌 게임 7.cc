a823948f7f8d7ddae8c981eab3d6174f9ac928e6
tmdahr <p84800384@gmail.com>
2025-08-26 15:14:27 +0900

    [Gold II] Title: 돌 게임 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
+++ 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
@@ -3,7 +3,7 @@
 int main(void)
 {
     long long N;
-    if (scanf("%lld", &N) != 1) return 0;
+    if(scanf("%lld", &N) != 1) return 0;
     int r = N % 5;
     if(r == 0 || r == 2) printf("CY\n");
     else printf("SK\n");
