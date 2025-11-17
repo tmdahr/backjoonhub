b365457a067ee3303fc08c9fa41a289901126051
tmdahr <p84800384@gmail.com>
2025-11-18 05:12:31 +0900

    [Gold I] Title: 도로, Time: 0 ms, Memory: 1228 KB -BaekjoonHub

--- 백준/Gold/1045. 도로/도로.cc
+++ 백준/Gold/1045. 도로/도로.cc
@@ -1,15 +1,12 @@
 #include <cstdio>
 #include <queue>
 #include <vector>
-
 using namespace std;
 
 int parent[55];
 
 int my_par(int x) {
-    while (x != parent[x]) {
-        x = parent[x];
-    }
+    while(x != parent[x]) x = parent[x];
     return x;
 }
 
@@ -17,7 +14,7 @@ void union_set(int x, int y) {
     int par_x = my_par(x);
 
     // compress path while climbing y
-    while (y != parent[y]) {
+    while(y != parent[y]) {
         int temp = y;
         y = parent[y];
         parent[temp] = par_x;
@@ -25,60 +22,53 @@ void union_set(int x, int y) {
     parent[y] = par_x;
 }
 
-int main() {
+int main(void) {
     int n, m;
     scanf("%d %d", &n, &m);
 
     char maps[55][55];
-    for (int i = 0; i < n; i++) {
-        scanf("%s", maps[i]);
-    }
+    for(int i=0; i<n; i++) scanf("%s", maps[i]);
 
     // priority queue for (i, j)
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> trash;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            if (maps[i][j] == 'Y') {
-                pq.push({i, j});
-            }
-        }
-    }
+    for(int i=0; i<n; i++)
+        for(int j=i; j<n; j++) 
+            if(maps[i][j] == 'Y') pq.push({i, j});
 
-    if ((int)pq.size() < m) {
+    if((int)pq.size() < m) {
         printf("-1\n");
         return 0;
     }
 
-    for (int i = 0; i < n; i++) parent[i] = i;
+    for(int i=0; i<n; i++) parent[i] = i;
 
     int answer[55] = {0};
     int cnt = 0;
 
-    while (!pq.empty()) {
+    while(!pq.empty()) {
         auto top = pq.top();
         pq.pop();
 
         int i = top.first;
         int j = top.second;
 
-        if (my_par(i) != my_par(j)) {
+        if(my_par(i) != my_par(j)) {
             union_set(i, j);
             answer[i]++;
             answer[j]++;
             cnt++;
-        } else {
-            trash.push({i, j});
         }
+        else trash.push({i, j});
     }
 
-    if (cnt != n - 1) {
+    if(cnt != n - 1) {
         printf("-1\n");
         return 0;
     }
 
-    for (int k = 0; k < m - cnt; k++) {
+    for(int k=0; k< m-cnt; k++) {
         auto top = trash.top();
         trash.pop();
         int i = top.first;
@@ -87,9 +77,7 @@ int main() {
         answer[j]++;
     }
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", answer[i]);
-    }
+    for(int i=0; i<n; i++) printf("%d ", answer[i]);
     printf("\n");
 
     return 0;
