a35069451143c691ac1b7f2683d9a6a135028771
tmdahr <p84800384@gmail.com>
2025-12-11 22:54:06 +0900

    [Bronze I] Title: 소수가 아닌 수 2, Time: 0 ms, Memory: 1116 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cc
+++ 백준/Bronze/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cc
@@ -1,20 +1,43 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
 
 int main(void) {
-    float k;
-    scanf("%f", &k);
+    char k[101];
+    scanf("%s", k);
+    
+    char frac[101];
+    strcpy(frac, k + 2);
 
-    int p = 1;
-    int q = 1;
+    int len = strlen(frac);
 
-    while(fabs((float)p / q - k) > 1e-6) {
-        if((float)p / q > k) q++;
-        else p++;
+    int numerator, denominator;
+
+    if (len == 1 || frac[0] != frac[len - 1]) {
+        numerator = atoi(frac);
+
+        denominator = 1;
+        for(int i=0; i<len; i++) denominator *= 10;
+    }
+    else {
+        numerator = atoi(frac);
+
+        denominator = 0;
+        for(int i=0; i<len; i++)
+            denominator = denominator * 10 + 9;
     }
 
+    int commonDivisor = gcd(numerator, denominator);
+    numerator /= commonDivisor;
+    denominator /= commonDivisor;
+
     printf("YES\n");
-    printf("%d %d\n", p, q);
+    printf("%d %d\n", numerator, denominator);
 
     return 0;
 }
