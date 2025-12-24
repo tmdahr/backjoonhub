f6f85a1bf99bef59b2bf6c14130f360c023f3472
tmdahr <p84800384@gmail.com>
2025-12-24 09:52:56 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int T;
+    scanf("%d", &T);
+
+    while(T--) {
+        long long A, B;
+        scanf("%lld %lld", &A, &B);
+        printf("yes\n");
+    }
+
+    return 0;
+}
