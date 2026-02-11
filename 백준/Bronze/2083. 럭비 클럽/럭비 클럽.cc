d58a2d50e7900542626c70a9a575c02157e60f64
tmdahr <p84800384@gmail.com>
2026-02-11 20:56:09 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    char name[25];
+    int age, weight;
+
+    while(1) {
+        scanf("%s %d %d", name, &age, &weight);
+        if(age == 0 && weight == 0 && name[0] == '#') break;
+        if(age > 17 || weight >= 80) printf("%s Senior\n", name);
+        else printf("%s Junior\n", name);
+    }
+    return 0;
+}
