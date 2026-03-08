24275ddfe7ea1e7e67bcca7a9bd4385bf54070e7
tmdahr <p84800384@gmail.com>
2026-03-09 08:07:51 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+
+    for(int i=1; i<=N; i++) {
+        for(int j=0; j<N-i; j++) printf(" ");
+        for(int j=0; j<i; j++) printf("*");
+        printf("\n");
+    }
+
+    for(int i=N-1; i>=1; i--) {
+        for(int j=0; j<N-i; j++) printf(" ");
+        for(int j=0; j<i; j++) printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
