b4156b847749083b1ccafe0e98c8a34ba08c17c2
tmdahr <p84800384@gmail.com>
2026-04-09 07:56:47 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int arr[3];
+
+    for(int i=0; i<3; i++) scanf("%d", &arr[i]);
+    
+    for(int i=0; i<3; i++)
+        for(int j=i+1; j<3; j++)
+            if(arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+    printf("%d %d %d\n", arr[0], arr[1], arr[2]);
+
+    return 0;
+}
